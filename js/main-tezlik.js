873377657f69051a30a7b9781306df5de772cf22
nurbek717 <nurbektukhtamishev@gmail.com>
2021-11-11 20:45:25 +0500

    sekund

--- js/main-tezlik.js
+++ js/main-tezlik.js
@@ -57,31 +57,34 @@ elForm.addEventListener('submit', function(evt){
     
     var bayBikeHour = elDistanceInput / bikeSpeed;
     var bayBikeMinut = Math.ceil((bayBikeHour - Math.floor(bayBikeHour))*60 )
-    
+    var bayBikeSekund = Math.floor((bayBikeMinut) * 60);
+
     var bayWalkeHour = elDistanceInput / walkSpeed;
     var bayWalkeMinut = Math.ceil((bayWalkeHour - Math.floor(bayWalkeHour))*60 )
-    
+    var bayWalkeSekund = Math.floor((bayWalkeMinut) * 60);
+
     var bayCarHour = elDistanceInput / carSpeed;
     var bayCarMinut = Math.ceil((bayCarHour - Math.floor(bayCarHour))*60 )
-    
+    var bayCarSekund = Math.floor((bayCarMinut) * 60);
+
     var bayPlaneHour = elDistanceInput / planeSpeed;
     var bayPlaneMinut = Math.ceil((bayPlaneHour - Math.floor(bayPlaneHour))*60 )
-    
+    var bayPlaneSekund = Math.floor((bayPlaneMinut) * 60);
     
     // bu cosole.log dagi ifodasi :
     
-    console.log(Math.floor(bayBikeHour) + " h " ,bayBikeMinut + " min ");
-    console.log(Math.floor(bayWalkeHour) + " h " ,bayWalkeMinut + " min ");
-    console.log(Math.floor(bayCarHour) + " h " ,bayCarMinut + " min ");
-    console.log(Math.floor(bayPlaneHour) + " h " ,bayPlaneMinut + " min ");
+    console.log(Math.floor(bayBikeHour) + " h " ,bayBikeMinut + " min " ,bayBikeSekund + " sekund ");
+    console.log(Math.floor(bayWalkeHour) + " h " ,bayWalkeMinut + " min " ,bayWalkeSekund + " sekund ");
+    console.log(Math.floor(bayCarHour) + " h " ,bayCarMinut + " min " ,bayCarSekund + " sekund ");
+    console.log(Math.floor(bayPlaneHour) + " h " ,bayPlaneMinut + " min " ,bayPlaneSekund + " sekund ");
     
     // bu text.Content :
 
     
-    elRezultWalk.textContent = Math.floor(bayWalkeHour) + " h  " + bayWalkeMinut + " min ";
-    elRezultBike.textContent = Math.floor(bayBikeHour) + " h " + bayBikeMinut + " min " ;
-    elRezultCar.textContent = Math.floor(bayCarHour) + " h " + bayCarMinut + " min ";
-    elRezultPlance.textContent = Math.floor(bayPlaneHour) + " h " + bayPlaneMinut + " min ";
+    elRezultWalk.textContent = Math.floor(bayWalkeHour) + " h  " + bayWalkeMinut + " min " + bayWalkeSekund + " sekund ";
+    elRezultBike.textContent = Math.floor(bayBikeHour) + " h " + bayBikeMinut + " min " + bayBikeSekund + " sekund " ;
+    elRezultCar.textContent = Math.floor(bayCarHour) + " h " + bayCarMinut + " min "  + bayCarSekund + " sekund ";
+    elRezultPlance.textContent = Math.floor(bayPlaneHour) + " h " + bayPlaneMinut + " min " + bayPlaneSekund + " sekund ";
   }
 
 
